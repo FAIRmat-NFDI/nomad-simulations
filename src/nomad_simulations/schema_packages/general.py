f62eb9821e1c83e6d09925bb50d3b4ac7c1548de
ndaelman <ndaelman@physik.hu-berlin.de>
2025-01-24 18:16:34 +0100

    Generate foundational base section, i.e. `ModelBaseSection`

--- src/nomad_simulations/schema_packages/general.py
+++ src/nomad_simulations/schema_packages/general.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 
 import numpy as np
 from nomad.config import config
-from nomad.datamodel.data import Schema
+from nomad.datamodel.data import Schema, ArchiveSection
 from nomad.datamodel.metainfo.basesections import Activity, Entity
 from nomad.metainfo import Datetime, Quantity, SchemaPackage, Section, SubSection
 
@@ -53,6 +53,41 @@ def check_normalized(func: 'Callable'):
         self._is_normalized = True
 
     return wrapper
+
+
+class ModelBaseSection(ArchiveSection):
+    """
+    Base class for the model sections.
+    Toggles the display name, as well as adds definition status.
+    """
+
+    name = Quantity(
+        type=str,
+        description="""
+        Display name of the model within the archive.
+        """,
+    )
+
+    iri = Quantity(
+        type=str,
+        shape=['*'],
+        description="""
+        The International Resource Identifier (IRI) of the model.
+        Can be used to link definitions from curated vocabularies or ontologies.
+        Use `http` or `https` specifier for active hyperlinks.
+        """,  # ? TODO: impose the format of the IRI
+    )
+
+    def name_from_section(self) -> str:
+        """Return the name of the section based on the class name."""
+        return ''.join(['_' + c.lower() if c.isupper() else c for c in self.__class__.__name__]).lstrip('_')
+    
+
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        super().normalize(archive, logger)
+        if not self.name:
+            self.name = self.name_from_section()
+
 
 
 class Program(Entity):
