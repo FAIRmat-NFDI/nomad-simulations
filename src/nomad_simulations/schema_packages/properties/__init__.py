05d6770ee1f80a4ddd882015372744c73394be27
ndaelman <ndaelman@physik.hu-berlin.de>
2025-02-03 11:58:39 +0100

    - Move out contribution setup to `decomposable.py` - Move out standalone property defintions to `properties/__init__.py`

--- src/nomad_simulations/schema_packages/properties/__init__.py
+++ src/nomad_simulations/schema_packages/properties/__init__.py
@@ -1,14 +1,9 @@
+import numpy as np
+from nomad.metainfo.metainfo import Quantity
+
 from .band_gap import ElectronicBandGap
 from .band_structure import ElectronicBandStructure, ElectronicEigenvalues, Occupancy
-from .energies import (
-    EnergyContribution,
-    FermiLevel,
-    KineticEnergy,
-    PotentialEnergy,
-    TotalEnergy,
-)
 from .fermi_surface import FermiSurface
-from .forces import BaseForce, ForceContribution, TotalForce
 from .greens_function import (
     ElectronicGreensFunction,
     ElectronicSelfEnergy,
@@ -24,20 +19,101 @@ from .spectral_profile import (
     SpectralProfile,
     XASSpectrum,
 )
-from .thermodynamics import (
-    ChemicalPotential,
-    Enthalpy,
-    Entropy,
-    GibbsFreeEnergy,
-    Heat,
-    HeatCapacity,
-    HelmholtzFreeEnergy,
-    Hessian,
-    InternalEnergy,
-    MassDensity,
-    Pressure,
-    Temperature,
-    VirialTensor,
-    Volume,
-    Work,
+
+
+energy = Quantity(
+    type=np.float64,
+    unit='joule',
+    description="""
+    The energy contribution.
+    """,
+)
+
+virial_tensor = Quantity(
+    type=np.float64,
+    unit='joule',
+    shape=['*', '*'],
+    description="""
+    A measure of the distribution of internal forces and the overall stress within
+    a system of particles. Mathematically, the virial tensor is defined as minus the sum
+    of the dot product between the position and force vectors for each particle.
+    The `VirialTensor` can be related to the non-ideal pressure of the system through
+    the virial theorem.
+    """,
+)
+
+force = Quantity(
+    type=np.float64,
+    unit='newton',
+    shape=['*'],
+    description="""
+    The force contribution.
+    """,
+)
+
+pressure = Quantity(
+    type=np.float64,
+    unit='pascal',
+    description="""
+    Force exerted per unit area.
+    """, # ? what area is referred to
+)
+
+volume = value = Quantity(
+    type=np.float64,
+    unit='m ** 3',
+    description="""
+    """,
+)  # ? hyper-volume
+
+mass_density = Quantity(
+    type=np.float64,
+    unit='kg / m ** 3',
+    description="""
+    Mass per unit volume of a material.
+    """,
+)
+
+temperature = Quantity(
+    type=np.float64,
+    unit='kelvin',
+    description="""
+    Measure of the average kinetic energy of the particles in a system.
+    """,
+)
+
+entropy = Quantity(
+    type=np.float64,
+    unit='joule / kelvin',
+    description="""
+    A measure of the disorder or randomness in a system.
+
+    From a thermodynamic perspective, `Entropy` is a measure of the system's energy
+    dispersal at a specific temperature, and can be interpreted as the unavailability of
+    a system's thermal energy for conversion into mechanical work. For a reversible
+    process, the change in `Entropy` is given mathematically by an integral over the
+    infinitesimal `Heat` (i.e., thermal energy transferred into the system) divided by the
+    `Temperature`.
+
+    From a statistical mechanics viewpoint, entropy quantifies the number of microscopic
+    configurations (microstates) that correspond to a thermodynamic system's macroscopic
+    state, as given by the Boltzmann equation for entropy.
+    """,
+) # ?
+
+heat_capacity = Quantity(
+    type=np.float64,
+    unit='joule / kelvin',
+    description="""
+    """,
+) # ?
+
+hessian = Quantity(
+    type=np.float64,
+    unit='joule / m ** 2',
+    shape=['*', '*'],
+    description="""
+    A square matrix of second-order partial derivatives of a potential energy function,
+    describing the local curvature of the energy surface.
+    """,
 )
---
+++ src/nomad_simulations/schema_packages/properties/decomposable.py
@@ -0,0 +1,76 @@
+from nomad.metainfo import placeholder, Quantity, SubSection, Reference
+from nomad_simulations.schema_packages.general import ModelBaseSection
+from nomad_simulations.schema_packages.model_method import BaseModelMethod
+from nomad_simulations.schema_packages.properties import energy, force
+
+class PropertyContribution(ModelBaseSection):
+    """
+    Abstract physical property section linking a property contribution to a contribution
+    from some method.
+
+    Abstract class for incorporating specific contributions of a physical property, while
+    linking this contribution to a specific component (of class `BaseModelMethod`) of the
+    over `ModelMethod` using the `model_method_ref` quantity.
+    """
+
+    value = placeholder
+
+    method_definition = Quantity(
+        type=Reference(BaseModelMethod),  # ! specialize to method definition
+        description="""
+        Further specification of the definition for the contribution, in terms of the method.
+        """,
+    )
+
+    def normalize(self, *args, **kwargs) -> None:
+        super().normalize(*args, **kwargs)
+        if not self.name:
+            self.name = self.get('model_method_ref').get('name')  # ? does this resolve references
+
+
+class DecomposableProperty(ModelBaseSection):
+    """
+    Abstract physical property section for decomposable properties.
+
+    Abstract class for incorporating decomposable physical properties, which can be
+    decomposed into contributions from different components (of class `BaseModelMethod`)
+    of the over `ModelMethod`.
+    """
+
+    value = placeholder
+
+    contributions = SubSection(sub_section=PropertyContribution.m_def, repeats=True)
+
+    # ! extract name from value
+
+
+class TotalEnergy(DecomposableProperty):
+    """
+    The total energy of a system. `contributions` specify individual energetic
+    contributions to the `TotalEnergy`.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.name = 'Total Energy'  # !
+
+    class EnergyContribution(PropertyContribution):
+        value = energy
+
+    contributions = SubSection(sub_section=EnergyContribution.m_def, repeats=True)
+
+
+class TotalForce(DecomposableProperty):
+    """
+    The total force on a system. `contributions` specify individual force
+    contributions to the `TotalForce`.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.name = 'Total Force'  # !
+
+    class ForceContribution(PropertyContribution):
+        value = force
+
+    contributions = SubSection(sub_section=ForceContribution.m_def, repeats=True)
