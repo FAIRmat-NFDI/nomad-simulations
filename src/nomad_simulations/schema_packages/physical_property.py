5a6c0c0600cc18a10f7dff0f9a429a4fa3bbc092
ndaelman <ndaelman@physik.hu-berlin.de>
2025-07-13 18:17:31 +0200

    - Remove old logic for enforcing shape - Mark unused or superfluous quantities

--- src/nomad_simulations/schema_packages/physical_property.py
+++ src/nomad_simulations/schema_packages/physical_property.py
@@ -1,72 +1,20 @@
-from functools import wraps
-from typing import TYPE_CHECKING
-
-import numpy as np
 from nomad import utils
 from nomad.datamodel.metainfo.plot import PlotSection
 from nomad.datamodel.metainfo.basesections.v2 import Entity
 from nomad.metainfo import URL, MEnum, Quantity, Reference, SectionProxy
 
-if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import EntryArchive
-    from nomad.metainfo import Context, Section
-    from structlog.stdlib import BoundLogger
-
 from nomad_simulations.schema_packages.model_method import BaseModelMethod
 from nomad_simulations.schema_packages.numerical_settings import SelfConsistency
-from nomad_simulations.schema_packages.variables import Variables
 
-# We add `logger` for the `validate_quantity_wrt_value` decorator
 logger = utils.get_logger(__name__)
-
-
-def validate_quantity_wrt_value(name: str = ''):
-    """
-    Decorator to validate the existence of a quantity and its shape with respect to the `PhysicalProperty.value`
-    before calling a method. An example can be found in the module `properties/band_structure.py` for the method
-    `ElectronicEigenvalues.order_eigenvalues()`.
-
-    Args:
-        name (str, optional): The name of the `quantity` to validate. Defaults to ''.
-    """
-
-    def decorator(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            # Checks if `quantity` is defined
-            quantity = getattr(self, name, None)
-            if quantity is None or len(quantity) == 0:
-                logger.warning(f'The quantity `{name}` is not defined.')
-                return False
-
-            # Checks if `value` exists and has the same shape as `quantity`
-            value = getattr(self, 'value', None)
-            if value is None:
-                logger.warning('The quantity `value` is not defined.')
-                return False
-            if value is not None and value.shape != quantity.shape:
-                logger.warning(
-                    f'The shape of the quantity `{name}` does not match the shape of the `value`.'
-                )
-                return False
-
-            return func(self, *args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
 
 class PhysicalProperty(PlotSection):
     """
-    A base section used to define the physical properties obtained in a simulation, experiment, or in a post-processing
-    analysis. The main quantity of the `PhysicalProperty` is `value`, whose instantiation has to be overwritten in the derived classes
-    when inheriting from `PhysicalProperty`. It contains `variables`, to define the variables over which the physical property varies (see variables.py).
-    This class can also store several string identifiers and quantities for referencing and establishing the character of a physical property.
+    A base section for computational output properties,
+    containing all (meta)data relevant for visualization.
+    Define and use `value` when the data corresponds with the titular section property.
+    May also contain partitions as subsections.
     """
-
-    # TODO add `errors`
-    # TODO add `smearing`
 
     name = Quantity(
         type=str,
@@ -79,8 +27,8 @@ class PhysicalProperty(PlotSection):
         type=URL,
         default='',
         description="""
-        Internationalized Resource Identifier (IRI) of the physical property defined in the FAIRmat
-        taxonomy, https://fairmat-nfdi.github.io/fairmat-taxonomy/.
+        Internationalized Resource Identifier (IRI) pointing to a definition,
+        typically within a larger, ontological framework.
         """,
     )
 
@@ -91,7 +39,7 @@ class PhysicalProperty(PlotSection):
         Source of the physical property. This quantity is related with the `Activity` performed to obtain the physical
         property. Example: an `ElectronicBandGap` can be obtained from a `'simulation'` or in a `'measurement'`.
         """,
-    )
+    )  # ? unused
 
     type = Quantity(
         type=str,
@@ -99,7 +47,6 @@ class PhysicalProperty(PlotSection):
         Type categorization of the physical property. Example: an `ElectronicBandGap` can be `'direct'`
         or `'indirect'`.
         """,
-        # ! add more examples in the description to improve the understanding of this quantity
     )
 
     label = Quantity(
@@ -108,13 +55,8 @@ class PhysicalProperty(PlotSection):
         Label for additional classification of the physical property. Example: an `ElectronicBandGap`
         can be labeled as `'DFT'` or `'GW'` depending on the methodology used to calculate it.
         """,
-        # ! add more examples in the description to improve the understanding of this quantity
     )
 
-    # variables = SubSection(sub_section=Variables.m_def, repeats=True)
-
-    # * `value` must be overwritten in the derived classes defining its type, unit, and description
-    # TODO use abstract to enforce policy?
     value: Quantity = None
 
     entity_ref = Quantity(
@@ -125,7 +67,7 @@ class PhysicalProperty(PlotSection):
             cell. In the first case, `outputs.model_system_ref` (see outputs.py) will point to the `ModelSystem` section,
             while in the second case, `entity_ref` will point to `AtomsState` section (see atoms_state.py).
         """,
-    )
+    )  # TODO: only used for electronic states, remove
 
     physical_property_ref = Quantity(
         type=Reference(SectionProxy('PhysicalProperty')),
@@ -153,7 +95,7 @@ class PhysicalProperty(PlotSection):
         Flag indicating whether the physical property is converged or not after a SCF process. This quantity is connected
         with `SelfConsistency` defined in the `numerical_settings.py` module.
         """,
-    )
+    )  # ? remove
 
     self_consistency_ref = Quantity(
         type=SelfConsistency,
@@ -161,61 +103,7 @@ class PhysicalProperty(PlotSection):
         Reference to the `SelfConsistency` section that defines the numerical settings to converge the
         physical property (see numerical_settings.py).
         """,
-    )
-
-    # @property
-    # def variables_shape(self) -> Optional[list]:
-    #    """
-    #    Shape of the variables over which the physical property varies. This is extracted from
-    #    `Variables.n_points` and appended in a list.
-    #
-    #    Example, a physical property which varies with `Temperature` and `ElectricField` will
-    #    return `variables_shape = [n_temperatures, n_electric_fields]`.
-    #
-    #    Returns:
-    #        (list): The shape of the variables over which the physical property varies.
-    #    """
-    #    if self.variables is not None:
-    #        return [v.get_n_points(logger) for v in self.variables]
-    #    return []
-
-    @property
-    def full_shape(self) -> list[int]:
-        """
-        Full shape of the physical property. This quantity is calculated as a concatenation of the `variables_shape`
-        and `value.shape`:
-
-            `full_shape = variables_shape + value.shape`
-
-        Example: a physical property which is a 3D vector and varies with `variables=[Temperature, ElectricField]`
-        will have `value.shape=[3]`, `variables_shape=[n_temperatures, n_electric_fields]`, and thus
-        `full_shape=[n_temperatures, n_electric_fields, 3]`.
-
-        Returns:
-            (list): The full shape of the physical property.
-        """
-        value_shape = self.value.shape if self.value is not None else []
-        # return self.variables_shape + value_shape
-        return [] + value_shape
-
-    def __init__(
-        self, m_def: 'Section' = None, m_context: 'Context' = None, **kwargs
-    ) -> None:
-        super().__init__(m_def, m_context, **kwargs)
-        # Checking if IRI is defined
-        if self.iri is None:
-            logger.warning(
-                'The used property is not defined in the FAIRmat taxonomy (https://fairmat-nfdi.github.io/fairmat-taxonomy/). You can contribute there if you want to extend the list of available materials properties.'
-            )
-
-    def __setattr__(self, name, value):
-        if name == 'value':
-            try:
-                value = np.array(value)
-                # self.__class__.value.shape = ['*'] * value.ndim
-            except Exception:
-                pass
-        return super().__setattr__(name, value)
+    )  # ? remove
 
     def _is_derived(self) -> bool:
         """
