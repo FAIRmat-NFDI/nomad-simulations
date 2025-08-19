cleaned composition formula, added new functionality for missing branch labels,  and fixed typing issue in tests, updated test results

src/nomad_simulations/schema_packages/model_system.py
@@ -1055,7 +1055,6 @@ class ModelSystem(System):
         Returns [] if any particle lacks a usable label/symbol.
         """
         logger = self.get_symbols.__annotations__['logger']
-
         if self._cache.get('symbols') is not None:
             return self._cache['symbols']
 
@@ -1467,6 +1466,7 @@ class ModelSystem(System):
         bond_list = root_bonds[mask]
         bond_list = np.unique(bond_list, axis=0)
 
+        # TODO - separate setting from getting
         if set_local:
             self.bond_list = bond_list
 