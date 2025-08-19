eb290351b6f249b5b1217f759bd251966402b1b8
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-08-19 17:05:46 +0200

    Move tb workflow out

--- src/nomad_simulations/schema_packages/workflow/__init__.py
+++ src/nomad_simulations/schema_packages/workflow/__init__.py
@@ -1,4 +1,3 @@
-from .beyond_dft import BeyondDFTResults, DFTTBWorkflow
 from .equation_of_state import EquationOfState
 from .general import ParallelWorkflow, SerialWorkflow, SimulationWorkflow
 from .geometry_optimization import GeometryOptimization
@@ -7,4 +6,5 @@ from .molecular_dynamics import MolecularDynamics
 from .phonon import Phonon
 from .photon_polarization import PhotonPolarizationWorkflow
 from .single_point import SinglePoint
+from .tb import DFTTBWorkflow
 from .thermodynamics import Thermodynamics
