5e21cdc54824f4f7570f6692c132c12600c364ab
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-08-19 16:49:47 +0200

    Add photon polarization workflow

--- src/nomad_simulations/schema_packages/workflow/__init__.py
+++ src/nomad_simulations/schema_packages/workflow/__init__.py
@@ -1,7 +1,9 @@
 from .beyond_dft import BeyondDFTResults, DFTGWWorkflow, DFTTBWorkflow
+from .equation_of_state import EquationOfState
 from .general import ParallelWorkflow, SerialWorkflow, SimulationWorkflow
 from .geometry_optimization import GeometryOptimization
 from .molecular_dynamics import MolecularDynamics
 from .phonon import Phonon
+from .photon_polarization import PhotonPolarizationWorkflow
 from .single_point import SinglePoint
 from .thermodynamics import Thermodynamics
