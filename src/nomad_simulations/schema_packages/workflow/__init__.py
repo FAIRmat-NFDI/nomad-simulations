ab1b382f55fb526752b6db0ba663297f60edc2d2
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-08-20 10:06:45 +0200

    Add maxent workflow def

--- src/nomad_simulations/schema_packages/workflow/__init__.py
+++ src/nomad_simulations/schema_packages/workflow/__init__.py
@@ -4,6 +4,7 @@ from .equation_of_state import EquationOfState
 from .general import ParallelWorkflow, SerialWorkflow, SimulationWorkflow
 from .geometry_optimization import GeometryOptimization
 from .gw import DFTGWWorkflow
+from .max_ent import DMFTMaxEntWorkflow
 from .molecular_dynamics import MolecularDynamics
 from .phonon import Phonon
 from .photon_polarization import PhotonPolarizationWorkflow
