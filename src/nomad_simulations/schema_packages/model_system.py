3dfea70f7db0e2618767720adf0cc621699c584b
jrudz <rudzinski@mpip-mainz.mpg.de>
2025-08-22 15:40:08 +0200

    ruff format

--- src/nomad_simulations/schema_packages/model_system.py
+++ src/nomad_simulations/schema_packages/model_system.py
@@ -1355,9 +1355,9 @@ class ModelSystem(System):
                 )
                 return
 
-            assert all(
-                0 <= i < n_particles for i in self.particle_indices
-            ), 'Invalid particle_indices in ModelSystem subsystem.'
+            assert all(0 <= i < n_particles for i in self.particle_indices), (
+                'Invalid particle_indices in ModelSystem subsystem.'
+            )
             return
 
         if parent.particle_indices is None:
@@ -1366,9 +1366,9 @@ class ModelSystem(System):
             )
             return
 
-        assert all(
-            pi in parent.particle_indices for pi in self.particle_indices
-        ), 'Invalid particle_indices in ModelSystem subsystem.'
+        assert all(pi in parent.particle_indices for pi in self.particle_indices), (
+            'Invalid particle_indices in ModelSystem subsystem.'
+        )
 
         # TODO logger.warning or logger.error in each case?
 
