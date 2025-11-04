2339f07250dac7455be5e46d7cd4d64dc9d1ac51
EBB2675 <esmaboydas4@gmail.com>
2025-11-04 13:32:18 +0100

    add coupled_cluster.py and configuration_interaction.py to workflows

---
+++ src/nomad_simulations/schema_packages/workflow/coupled_cluster.py
@@ -0,0 +1,52 @@
+# nomad_simulations/schema_packages/workflow/cc.py
+
+from nomad.datamodel import EntryArchive
+from nomad.metainfo import SchemaPackage
+from structlog.stdlib import BoundLogger
+
+from nomad_simulations.schema_packages.utils import log
+
+from .beyond_hf import BeyondHFModel, BeyondHFResults, BeyondHFWorkflow
+
+m_package = SchemaPackage()
+
+
+class HFCCModel(BeyondHFModel):
+    _label = 'HF+CC workflow parameters'
+
+
+class HFCCResults(BeyondHFResults):
+    _label = 'HF+CC workflow results'
+
+
+class HFCCWorkflow(BeyondHFWorkflow):
+    """
+    Definitions for Coupled-Cluster calculations based on HF (HF → CC).
+    """
+
+    @log
+    def map_inputs(self, archive: EntryArchive) -> None:
+        if not self.model:
+            self.model = HFCCModel()
+        logger = self.map_inputs.__annotations__['logger']
+        super().map_inputs(archive, logger=logger)
+
+    @log
+    def map_outputs(self, archive: EntryArchive) -> None:
+        if not self.results:
+            self.results = HFCCResults()
+        logger = self.map_outputs.__annotations__['logger']
+        super().map_outputs(archive, logger=logger)
+
+    def normalize(self, archive: EntryArchive, logger: BoundLogger) -> None:
+        """
+        Link the HF and CC single-point workflows in the HF-CC workflow.
+        """
+        super().normalize(archive, logger)
+
+        # Name the last task CC if it is unnamed
+        if self.tasks and not self.tasks[-1].name:
+            self.tasks[-1].name = 'CC'
+
+
+m_package.__init_metainfo__()
