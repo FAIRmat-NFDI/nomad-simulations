1b573f434bb22e05c5a4c6a6a1f76dc234ccd8a7
Nathan Daelman <107392603+ndaelman-hu@users.noreply.github.com>
2025-12-03 15:41:07 +0100

    Update src/nomad_simulations/schema_packages/numerical_settings.py
    reviewer: clarify language for quantum numbers
    
    Co-authored-by: Esma B. Boydas <80156958+EBB2675@users.noreply.github.com>

--- src/nomad_simulations/schema_packages/numerical_settings.py
+++ src/nomad_simulations/schema_packages/numerical_settings.py
@@ -1156,7 +1156,7 @@ class Pseudopotential(NumericalSettings):
         type=np.int32,
         shape=[],
         description="""
-        Maximum magnetic momentum of the pseudopotential projectors.
+        Maximum magnetic quantum number of the pseudopotential projectors.
         """,
     )
 
