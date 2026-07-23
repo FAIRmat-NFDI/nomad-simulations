e546d8e2cc4d9148be3bd3c3dc040a13476e3070
EBB2675 <esmaboydas4@gmail.com>
2026-07-23 16:59:58 +0200

    revert the style change

--- src/nomad_simulations/schema_packages/basis_set.py
+++ src/nomad_simulations/schema_packages/basis_set.py
@@ -115,7 +115,7 @@ class PlaneWaveBasisSet(BasisSetComponent, KMesh):
         if cutoff_energy is None:
             return None
         m_e = const.m_e * ureg(const.unit('electron mass'))
-        hbar = const.hbar * ureg(const.unit('reduced Planck constant'))
+        hbar = const.hbar * ureg.joule * ureg.second
         return np.sqrt(2 * m_e * cutoff_energy) / hbar
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
